2c55ed204a794c1f4f6aa6e9cd032d113cb1a88d
KunYi Chen <kunyi.chen@gmail.com>
2019-06-02 17:01:42 +0800

    complete lesson10, but fast method is '+' on node v10.15.3
    my test result, node v10.15.3 on Windows 10
    
    + x 579,554,398 ops/sec ±1.85% (89 runs sampled)
    parseInt x 193,754,622 ops/sec ±1.76% (93 runs sampled)
    Number x 118,120,617 ops/sec ±2.60% (88 runs sampled)
    Fastest is +

---
+++ lesson10/main.js
@@ -0,0 +1,37 @@
+var Benchmark = require('benchmark');
+
+var int1 = function (str) {
+    return +str;
+};
+  
+var int2 = function (str) {
+    return parseInt(str, 10);
+};
+  
+var int3 = function (str) {
+    return Number(str);
+};
+
+var suite = new Benchmark.Suite;
+
+var number = '100';
+
+suite
+.add('+', function() {
+  int1(number);
+})
+.add('parseInt', function() {
+  int2(number);
+})
+.add('Number', function () {
+  int3(number);
+})
+
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+
+.run({ 'async': true });
